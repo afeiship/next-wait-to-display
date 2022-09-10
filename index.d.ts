2d11f29d01cab3003173bed197c585d26f7bf19d
afeiship <1290657123@qq.com>
2022-09-10 16:20:26 +0800

    feat: add typing

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+interface NxStatic {
+  /**
+   * Wait element display in document.
+   * @param selector
+   * @param delay
+   * @param callback
+   */
+  waitToDisplay(selector: string, delay: number, callback: any): any;
+}
