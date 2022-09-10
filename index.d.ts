00f78d36b381f4fd6932106a9d442d0ae7c1a4bf
afeiship <1290657123@qq.com>
2022-09-10 23:47:59 +0800

    wip

--- index.d.ts
+++ index.d.ts
@@ -2,9 +2,9 @@ interface NxStatic {
   /**
    * Wait element display in document.
    * @param selector The selector to query.
-   * @param delay default 100ms
-   * @param callback callback function
-   * @param timeout 0 means no timeout
+   * @param delay default 100ms.
+   * @param callback callback function.
+   * @param times Times.
    */
   waitToDisplay(selector: string, delay: number, callback: any, times?: number): any;
 }
